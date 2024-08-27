06b7b12e0b61b4fbe62ed0333468d1bb349670f9
Danrley <danrley.barross@gmail.com>
2024-08-26 21:36:15 -0300

    create enviroment

---
+++ src/DemoCleanArchitecture/DemoCleanArchitecture.Infrastructure/PostgresDataAccess/AutoMapperProfile/InfraDomainProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace DemoCleanArchitecture.Infrastructure.PostgresDataAccess.AutoMapperProfile
+{
+    public class InfraDomainProfile : Profile
+    {
+        public InfraDomainProfile()
+        {
+            CreateMap<Entities.Customer.Customer, Domain.Customer.Customer>().ReverseMap();
+
+            CreateMap<Entities.Bank.Bank, Domain.Bank.Bank>().ReverseMap();
+
+        }
+    }
+}
---
+++ src/DemoCleanArchitecture/DemoCleanArchitecture.WebApi/Modules/WebApiModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using DemoCleanArchitecture.WebApi.UseCases.Bank;
+using DemoCleanArchitecture.WebApi.UseCases.Customer;
+
+namespace DemoCleanArchitecture.WebApi.Modules
+{
+    public class WebApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(Startup).Assembly)
+                .AsSelf().InstancePerLifetimeScope();
+
+            builder.RegisterType<CustomerPresenter>().As<Application.Boundaries.Customer.IOutputPort>().AsSelf().InstancePerLifetimeScope();
+
+            builder.RegisterType<BankPresenter>().As<Application.Boundaries.Bank.IOutputPort>().AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
